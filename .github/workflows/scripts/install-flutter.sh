594348c530097358537ef5d462543947b6654bd7
Salakar <mike.diarmid@gmail.com>
2020-10-03 15:44:50 +0100

    -

--- .github/workflows/scripts/install-flutter.sh
+++ .github/workflows/scripts/install-flutter.sh
@@ -2,9 +2,9 @@
 
 BRANCH=$1
 
+cd $HOME
 git clone https://github.com/flutter/flutter.git --depth 1 -b $BRANCH _flutter
-
-echo "::add-path::$GITHUB_WORKSPACE/_flutter/bin"
+echo "::add-path::$HOME/_flutter/bin"
 echo "::add-path::$HOME/.pub-cache/bin"
-echo "::add-path::$GITHUB_WORKSPACE/_flutter/.pub-cache/bin"
-echo "::add-path::$GITHUB_WORKSPACE/_flutter/bin/cache/dart-sdk/bin"
+echo "::add-path::$HOME/_flutter/.pub-cache/bin"
+echo "::add-path::$HOME/_flutter/bin/cache/dart-sdk/bin"
