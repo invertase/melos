1f8338df76be409c43fe09dc62591610ee9e18d5
Salakar <mike.diarmid@gmail.com>
2020-10-03 16:45:40 +0100

    -

--- .github/workflows/scripts/install-flutter.bat
+++ .github/workflows/scripts/install-flutter.bat
@@ -3,8 +3,13 @@ CD /D %systemdrive%%homepath%
 git clone https://github.com/flutter/flutter.git --depth 1 -b %BRANCH% _flutter
 
 %systemdrive%%homepath%\_flutter\bin\flutter doctor
+%systemdrive%%homepath%\_flutter\bin\dart --version
 
-ECHO "##[add-path]%systemdrive%%homepath%\_flutter\bin"
-ECHO "##[add-path]%LOCALAPPDATA%\Pub\Cache\bin"
-ECHO "##[add-path]%systemdrive%%homepath%\_flutter\Pub\Cache\bin"
-ECHO "##[add-path]%systemdrive%%homepath%\_flutter\bin\cache\dart-sdk\bin"
+ECHO "##[add-path]%systemdrive%%homepath%\\_flutter\\bin"
+ECHO "##[add-path]%systemdrive%%homepath%\\_flutter\\bin\\cache\\dart-sdk\\bin"
+ECHO "##[add-path]%systemdrive%%homepath%\\_flutter\\Pub\\Cache\\bin"
+ECHO "##[add-path]%LOCALAPPDATA%\\Pub\\Cache\\bin"
+
+
+DIR %systemdrive%%homepath%\_flutter\bin
+DIR %systemdrive%%homepath%\_flutter\bin\cache\dart-sdk\bin
