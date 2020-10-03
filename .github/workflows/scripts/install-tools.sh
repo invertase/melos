d0aeae06d0fa380ea1d25ddb28524df0a3f37938
Salakar <mike.diarmid@gmail.com>
2020-10-03 15:32:35 +0100

    -

--- .github/workflows/scripts/install-tools.sh
+++ .github/workflows/scripts/install-tools.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pub global activate --source=path .
 echo "::add-path::$HOME/.pub-cache/bin"
 echo "::add-path::$GITHUB_WORKSPACE/_flutter/.pub-cache/bin"
 echo "::add-path::$GITHUB_WORKSPACE/_flutter/bin/cache/dart-sdk/bin"
+pub global activate --source=path ./packages/melos
