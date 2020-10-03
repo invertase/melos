e8a386c928d19ee0070d1280efa527a66c44b994
Salakar <mike.diarmid@gmail.com>
2020-10-03 17:03:36 +0100

    -

--- .github/workflows/scripts/install-tools.bat
+++ .github/workflows/scripts/install-tools.bat
@@ -1,7 +1,5 @@
 CD packages\melos\
-dart pub get
-dart pub global activate --source=path .
+CMD /K dart pub global activate --source=path .
 CD /D %GITHUB_WORKSPACE%
-
 melos bootstrap
 ECHO "Bootstrap Completed"
