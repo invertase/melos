3321359c2d7102bbdc578a39a8f4f3c9c969479b
Salakar <mike.diarmid@gmail.com>
2020-10-03 16:18:01 +0100

    -

--- .github/workflows/scripts/install-flutter.bat
+++ .github/workflows/scripts/install-flutter.bat
@@ -2,7 +2,7 @@ echo off
 set BRANCH=%1
 echo on
 cd %systemdrive%%homepath%
-git clone https://github.com/flutter/flutter.git --depth 1 -b $BRANCH _flutter
+git clone https://github.com/flutter/flutter.git --depth 1 -b %BRANCH% _flutter
 
 echo "::add-path::%systemdrive%%homepath%\_flutter\bin"
 echo "::add-path::%LOCALAPPDATA%\Pub\Cache\bin"
