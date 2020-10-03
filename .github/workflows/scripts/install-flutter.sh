735a9e92a99bc720797a29174241385bf04b9560
Salakar <mike.diarmid@gmail.com>
2020-10-03 15:37:06 +0100

    -

--- .github/workflows/scripts/install-flutter.sh
+++ .github/workflows/scripts/install-flutter.sh
@@ -4,3 +4,6 @@ BRANCH=$1
 
 git clone https://github.com/flutter/flutter.git --depth 1 -b $BRANCH _flutter
 echo "::add-path::$GITHUB_WORKSPACE/_flutter/bin"
+echo "::add-path::$HOME/.pub-cache/bin"
+echo "::add-path::$GITHUB_WORKSPACE/_flutter/.pub-cache/bin"
+echo "::add-path::$GITHUB_WORKSPACE/_flutter/bin/cache/dart-sdk/bin"
--- .github/workflows/scripts/install-tools.sh
+++ .github/workflows/scripts/install-tools.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo "::add-path::$HOME/.pub-cache/bin"
-echo "::add-path::$GITHUB_WORKSPACE/_flutter/.pub-cache/bin"
-echo "::add-path::$GITHUB_WORKSPACE/_flutter/bin/cache/dart-sdk/bin"
-pub global activate --source=path ./packages/melos
+cd packages/melos
+pub get
+pub global activate --source=path .
+cd ../..
+melos bootstrap
