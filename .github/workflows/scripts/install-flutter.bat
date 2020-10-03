474b97b7aa5c1a114907281b8dfcf708f0c702ce
Salakar <mike.diarmid@gmail.com>
2020-10-03 16:13:30 +0100

    -

---
+++ .github/workflows/scripts/install-flutter.bat
@@ -0,0 +1,10 @@
+echo off
+set BRANCH=%1
+echo on
+cd %systemdrive%%homepath%
+git clone https://github.com/flutter/flutter.git --depth 1 -b $BRANCH _flutter
+
+echo "::add-path::%systemdrive%%homepath%\_flutter\bin"
+echo "::add-path::%systemdrive%%homepath%/%LOCALAPPDATA%\Pub\Cache\bin"
+echo "::add-path::%systemdrive%%homepath%\_flutter\.pub-cache\bin"
+echo "::add-path::%systemdrive%%homepath%\_flutter\bin\cache\dart-sdk\bin"
---
+++ .github/workflows/scripts/install-tools.bat
@@ -0,0 +1,5 @@
+cd packages\melos\
+dart pub get
+dart pub global activate --source=path .
+cd ..\..\
+melos bootstrap
