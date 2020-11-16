f41db8aeaee96bd0c77fddda09422787f9defee6
Chris Yang <ychris@google.com>
2020-11-16 14:31:39 -0800

    format

--- packages/melos/lib/src/command/version.dart
+++ packages/melos/lib/src/command/version.dart
@@ -63,8 +63,8 @@ class VersionCommand extends Command {
         help:
             'By default, melos version will commit changes to pubspec.yaml files and tag the release. Pass --no-git-tag-version to disable the behavior.');
     argParser.addFlag('yes',
-       defaultsTo: false,
-       help: 'Skips the Y/N prompt at the beginning of the command');
+        defaultsTo: false,
+        help: 'Skips the Y/N prompt at the beginning of the command');
   }
 
   @override
