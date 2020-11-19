b855638d9d5c0d9303de61fb6366648e1ac25aac
Salakar <mike.diarmid@gmail.com>
2020-11-19 17:47:08 +0000

    chore: switch to new GitHub action path syntax

--- .github/workflows/scripts/install-flutter.sh
+++ .github/workflows/scripts/install-flutter.sh
@@ -4,7 +4,7 @@ BRANCH=$1
 
 cd $HOME
 git clone https://github.com/flutter/flutter.git --depth 1 -b $BRANCH _flutter
-echo "::add-path::$HOME/_flutter/bin"
-echo "::add-path::$HOME/.pub-cache/bin"
-echo "::add-path::$HOME/_flutter/.pub-cache/bin"
-echo "::add-path::$HOME/_flutter/bin/cache/dart-sdk/bin"
+echo "$HOME/_flutter/bin" >> $GITHUB_PATH
+echo "$HOME/.pub-cache/bin" >> $GITHUB_PATH
+echo "$HOME/_flutter/.pub-cache/bin" >> $GITHUB_PATH
+echo "$HOME/_flutter/bin/cache/dart-sdk/bin" >> $GITHUB_PATH
