5cc983559c38300636e4c647bca0813f0943acd6
Scott Hyndman <scott@madewithfelt.com>
2021-03-26 00:26:44 -0400

    feat: Add melos.yaml command configuration
    melos.yaml can now contain a `command` section, which is a map of
    command-specific configuration objects. For example:
    
    ```yaml
    name: my-mono-repo
    packages:
    - packages/**
    command:
      version: # Configuration for version command
        message: Here's a commit message!
      publish: # Configuration for publish command
        tagPrefix: v
    ```
    
    This creates configurations for two commands, 'version' and 'publish',
    which are accessible to those commands through their `commandConfig`
    property (defined on the `MelosConfig` base class).

--- packages/melos/lib/src/command/base.dart
+++ packages/melos/lib/src/command/base.dart
@@ -2,8 +2,17 @@ import 'package:args/args.dart';
 import 'package:args/command_runner.dart';
 
 import '../common/utils.dart';
+import '../common/workspace.dart';
+import '../common/workspace_command_config.dart';
 
 abstract class MelosCommand extends Command {
+  /// The `melos.yaml` configuration for this command.
+  ///
+  /// This is the configuration under the `melos.yaml`'s `command.{name}` field.
+  /// If none exists, a configuration object with no contents is returned.
+  MelosCommandConfig get commandConfig =>
+      currentWorkspace.config.commands.configForCommandNamed(name);
+
   /// Overridden to support line wrapping when printing usage.
   @override
   ArgParser get argParser =>
