c200a1d576659e7c1b9b3698f81b32b19f6de14d
Scott Hyndman <scott@madewithfelt.com>
2021-03-31 02:56:13 -0400

    refactor: Clean up the MelosWorkspace, and ensure a package catch-all
    allPackages is a new field on MelosWorkspace that refers to _all_
    packages matched by `melos.yaml`'s `packages` glob.
    
    Additionally, this commit:
    * Uses Globs that respect IOOverrides
    * Extracts a common package load function on MelosWorkspace
    * Ensures default values in the Workspace's filter load method
    * Makes styling a bit more consistent

---
+++ packages/melos/lib/src/common/glob.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:glob/glob.dart';
+import 'package:path/path.dart' as p;
+
+/// Returns a [Glob] configured to work in both production and test
+/// environments.
+///
+/// Workaround for https://github.com/dart-lang/glob/issues/52
+Glob createGlob(
+  String pattern, {
+  p.Context context,
+  bool recursive = false,
+  bool caseSensitive,
+}) {
+  context ??= p.Context(
+    style: p.context.style,
+    // This ensures that IOOverrides are taken into account when determining the
+    // current working directory used by the Glob.
+    //
+    // See https://github.com/dart-lang/glob/issues/52 for more information.
+    current: Directory.current.path,
+  );
+  return Glob(
+    pattern,
+    context: context,
+    recursive: recursive,
+    caseSensitive: caseSensitive,
+  );
+}
