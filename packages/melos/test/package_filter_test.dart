5016dbfb61be764e304f9f77ddbbd71e20b2636b
Remi Rousselet <darky12s@gmail.com>
2021-06-21 23:39:48 +0100

    Add fileExists/dirExists e2e tests

---
+++ packages/melos/test/package_filter_test.dart
@@ -0,0 +1,82 @@
+import 'dart:io';
+
+import 'package:melos/melos.dart';
+import 'package:path/path.dart';
+import 'package:pubspec/pubspec.dart';
+import 'package:test/test.dart';
+
+import 'utils.dart';
+
+void main() {
+  group('PackageFilter', () {
+    test('dirExists', () async {
+      final workspaceDir = createTemporaryWorkspaceDirectory();
+
+      final aDir = await createProject(
+        workspaceDir,
+        const PubSpec(name: 'a'),
+      );
+      Directory(join(aDir.path, 'test')).createSync();
+
+      await createProject(
+        workspaceDir,
+        const PubSpec(name: 'b'),
+      );
+
+      final workspace = await MelosWorkspace.fromDirectory(
+        workspaceDir,
+        logger: TestLogger(),
+        filter: PackageFilter(
+          dirExists: ['test'],
+        ),
+      );
+
+      expect(
+        workspace.allPackages.values,
+        [
+          isA<Package>().having((p) => p.name, 'name', 'a'),
+          isA<Package>().having((p) => p.name, 'name', 'b'),
+        ],
+      );
+      expect(
+        workspace.filteredPackages.values,
+        [isA<Package>().having((p) => p.name, 'name', 'a')],
+      );
+    });
+
+    test('fileExists', () async {
+      final workspaceDir = createTemporaryWorkspaceDirectory();
+
+      final aDir = await createProject(
+        workspaceDir,
+        const PubSpec(name: 'a'),
+      );
+      File(join(aDir.path, 'log.txt')).createSync();
+
+      await createProject(
+        workspaceDir,
+        const PubSpec(name: 'b'),
+      );
+
+      final workspace = await MelosWorkspace.fromDirectory(
+        workspaceDir,
+        logger: TestLogger(),
+        filter: PackageFilter(
+          fileExists: ['log.txt'],
+        ),
+      );
+
+      expect(
+        workspace.allPackages.values,
+        [
+          isA<Package>().having((p) => p.name, 'name', 'a'),
+          isA<Package>().having((p) => p.name, 'name', 'b'),
+        ],
+      );
+      expect(
+        workspace.filteredPackages.values,
+        [isA<Package>().having((p) => p.name, 'name', 'a')],
+      );
+    });
+  });
+}
