74974d6391cc66598612a4715bfb816c926ee475
huang12zheng <805104533@qq.com>
2021-12-07 02:26:23 +0800

    test: remove no used package

--- packages/melos/test/commands/run_test.dart
+++ packages/melos/test/commands/run_test.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:melos/melos.dart';
 import 'package:melos/src/common/glob.dart';
-import 'package:melos/src/common/utils.dart';
 import 'package:melos/src/scripts.dart';
 import 'package:path/path.dart';
 import 'package:pubspec/pubspec.dart';
