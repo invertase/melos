87c2a4a1606b63265c0b2deb36c818c3c39178e0
Salakar <mike.diarmid@gmail.com>
2022-01-04 04:38:16 +0000

    chore: remove `ansi_styles` package - has moved to dart-cli-utilties repository

--- packages/ansi_styles/lib/src/supports_ansi.dart
+++
@@ -1,2 +0,0 @@
-/// Whether the current environment supports styling via ansi escapes.
-bool get supportsAnsiColor => true;
--- packages/ansi_styles/lib/src/supports_ansi_io.dart
+++
@@ -1,4 +0,0 @@
-import 'dart:io' as io;
-
-/// Whether the current environment supports styling via ansi escapes.
-bool get supportsAnsiColor => io.stdout.supportsAnsiEscapes;
