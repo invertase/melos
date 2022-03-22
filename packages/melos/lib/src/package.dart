8fd2d4aea8127fed23cadabf6de45de3b65d6d1c
Eka Setiawan Saputra <mr.poetra22@gmail.com>
2022-03-23 00:20:17 +0700

    fix: regex example

--- packages/melos/lib/src/package.dart
+++ packages/melos/lib/src/package.dart
@@ -105,7 +105,7 @@ RegExp dependencyVersionReplaceRegex(String dependencyName) {
   );
 }
 
-// https://regex101.com/r/ob8xRv/1
+// https://regex101.com/r/HIeQaI/1
 RegExp hostedDependencyVersionReplaceRegex(String dependencyName) {
   return RegExp(
     '''(^[ \t]*?(?<dependency>$dependencyName)[ \\t]*?:[ \\t]*?[\\s\\S]*?[ \\t]*?version:[ \\t]*?)(?<version>any|\\^.*|["'^<>=]*\\d\\.\\d\\.\\d['"._ \\t<>=\\d-\\w+]*|\$)\$''',
