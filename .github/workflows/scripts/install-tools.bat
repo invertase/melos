17dfde760c538877c2fc65525df6db9b6da9c0f7
Gabriel Terwesten <gabriel@terwesten.net>
2022-09-20 10:43:49 +0200

    tool: enable `usePubspecOverrides` in Melos repo

--- .github/workflows/scripts/install-tools.bat
+++ .github/workflows/scripts/install-tools.bat
@@ -1,2 +1,4 @@
-CMD /K dart pub global activate --source=path . --executable=melos
+CMD /C dart pub global activate --source=path . --executable=melos
+REM Workaround an issue when running global executables on Windows for the first time.
+CMD /C melos > NUL
 melos bootstrap
