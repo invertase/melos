c904030f10ea3732fa5f7ce4c780de8f6424382b
Gabriel Terwesten <gabriel@terwesten.net>
2023-02-04 21:16:41 +0100

    Fixup

--- .github/workflows/scripts/install-tools.bat
+++ .github/workflows/scripts/install-tools.bat
@@ -1,2 +1,4 @@
 CMD /C dart pub global activate --source=path . --executable=melos --overwrite
+REM Workaround an issue when running global executables on Windows for the first time.
+CMD /C melos > NUL
 melos bootstrap
