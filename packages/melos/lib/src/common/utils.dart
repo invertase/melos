4f3564f461d82e78143ff9743b1e3d53ef938cb2
Jessica Tarra <jessica@tarralva.com>
2024-06-23 15:56:59 -0300

    refactor: implement persistent shell for improved command execution

--- packages/melos/lib/src/common/utils.dart
+++ packages/melos/lib/src/common/utils.dart
@@ -73,6 +73,10 @@ final melosPackageUri = Uri.parse('package:melos/melos.dart');
 final _camelCasedDelimiterRegExp = RegExp(r'[_\s-]+');
 
 extension StringUtils on String {
+  String addStepPrefixEmoji() {
+    return '➡️ step: $this';
+  }
+
   String indent(String indent) {
     final split = this.split('\n');
 
@@ -203,6 +207,77 @@ class PromptException extends MelosException {
 
   @override
   String toString() => 'Was unable to prompt for input:\n$message';
+}
+
+class PersistentShell {
+  PersistentShell({
+    required MelosLogger logger,
+    String? workingDirectory,
+  }) {
+    _logger = logger;
+    _workingDirectory = workingDirectory;
+  }
+
+  final _isWindows = currentPlatform.isWindows;
+  late final MelosLogger _logger;
+  late final String? _workingDirectory;
+  late Process _process;
+  /// This list is intended to store the commands that are sent to the shell.
+  /// Currently, it is not being utilized in the code,
+  ///TODO: remove this or actually use it
+  final List<String> _commands = [];
+
+  Future<void> startShell() async {
+    final executable = _isWindows ? 'cmd.exe' : '/bin/sh';
+
+    _process = await Process.start(
+      executable,
+      _isWindows ? ['/C'] : [],
+      workingDirectory: _workingDirectory,
+    );
+
+    _process.stdout.listen(
+      (event) {
+        _logger.logWithoutNewLine(utf8.decode(event, allowMalformed: true));
+      },
+      onDone: () {
+        /// TODO: Identify and log the specific steps that have been completed
+        /// successfully.
+        _logger.success('Shell process completed some steps successfully.');
+      },
+    );
+    _process.stderr.listen(
+      (event) {
+        _logger.error(utf8.decode(event, allowMalformed: true));
+      },
+      onDone: () {
+        /// TODO: Identify and log the specific steps that have completed
+        /// with errors.
+        _logger.error('Shell process completed with errors.');
+      },
+    );
+  }
+
+  void sendCommand(String command) {
+    _commands.add(command);
+
+    final formattedScriptStep = targetStyle(
+      command.addStepPrefixEmoji().withoutTrailing('\n'),
+    );
+
+    final echoCommand = 'echo "$formattedScriptStep"';
+
+    final fullCommand = _isWindows
+        ? '$echoCommand && $command'
+        : 'eval "$echoCommand && $command"';
+
+    _process.stdin.writeln(fullCommand);
+  }
+
+  Future<void> stopShell() async {
+    await _process.stdin.close();
+    await _process.exitCode;
+  }
 }
 
 String promptInput(
