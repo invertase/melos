c80c8e82c80bb9283786f4f247da49860262893f
mahmut.taskiran <mahmut.taskiran@talabat.com>
2024-12-21 19:27:05 +0400

    added 'no dependencies would change in' case to the matcher

--- packages/melos/test/matchers.dart
+++ packages/melos/test/matchers.dart
@@ -24,6 +24,7 @@ Matcher ignoringDependencyMessages(String expected) {
                 !line.startsWith('Got dependencies!') &&
                 // Removes lines like "  pub_updater 0.4.0 (0.5.0 available)"
                 !(line.startsWith('  ') && line.contains(' available)')) &&
+                !line.startsWith('No dependencies would change in') &&
                 !line.contains(
                   'newer versions incompatible with dependency constraints',
                 ) &&
